143097abfd25175d0c0f45ccf4d7d5f6bf3ad637
DanZ <danrzurawski@gmail.com>
2017-06-16 13:45:45 -0500

    Fixed formatting. Added get_output_data function

--- InputModelOutput.py
+++ InputModelOutput.py
@@ -1,23 +1,54 @@
-# -*- coding: utf-8 -*-
 """
-Created on Fri Jun 16 10:43:05 2017
 
-@author: kesh1_000
+    @organization: Fermilab
+    @author: Daniel Zurawski
+    @author: Keshav Kapoor
+    
+    Created on Fri June 16 10:43:05 2017
 """
 
 import pandas as pd
 import numpy as np
 
-def getInputData(filename):
-    #Takes a csv file with headers: id, z, phi, eta, val, r, zl
-    #Returns a 3D array with values z, phi, r to put into a network(as input)
+def get_input_data(dataframe):
+    """ Retrieve the input data in the correct format from 'dataframe'.
+        @param  dataframe :: pd.DataFrame
+                    A data frame with headers: (id, z, phi, eta, val, r, zl).
+        @return np.array 2-D
+                    A matrix with columns of data: (r, phi, z)
+                    
+    """
+    return dataframe[["r", "phi", "z"]].values
+
+def get_output_data(dataframe):
+    """ Retrieve the output data in the correct format form 'dataframe'.
+        @param  dataframe :: pd.DataFrame
+                    A data frame with headers: (id, z, phi, eta, val, r, zl).
+        @return np.array 2-D
+                    A matrix where a given column at index i is the probability
+                    vector of which track the particle at index i belongs to.
+    """
+    tracks  = dataframe[["id"]].values[:,0]
+    uniques = np.unique(tracks)
+    matrix  = np.zeros((tracks.size, uniques.size))
     
-    initData = pd.read_csv(filename)
-    print(initData)
+    track2row = dict() 
+    for i, track in enumerate(uniques):
+        track2row[track] = i
+     
+    for i, track in enumerate(tracks):
+        matrix[i, track2row[track]] = 1
+        
+    return matrix
+
+def main():
+    filename  = ("linear_data_1.csv")
+    dataframe = pd.read_csv(filename)
+    in_data   = get_input_data (dataframe)
+    out_data  = get_output_data(dataframe)
     
-    #Remove id, eta, val, and z
-    refData = initData.drop(['id', 'eta', 'val', 'zl'], axis=1).values
-    return refData
-# HELLO THERE! (Daniel)
-data = getInputData("file_o_stuff3.csv")
-print(data)
+    print(out_data.shape)
+
+print("=== The program is starting. ===")
+main()
+print("=== The program is ending. ===")
